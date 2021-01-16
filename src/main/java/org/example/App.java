fa7f637adeed21d50dd3eb5ef79726bb2a73f5a5
cmada <madalina.ciceu@gmail.com>
2021-01-16 21:48:45 +0200

    overloaded calculator methods

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -21,7 +21,27 @@ public class App
         System.out.println(" The product is " + product);
 
         System.out.println(" Dividing " + x + " and " + y);
-        double result = calculator.divide(x,y);
+        int result = calculator.divide(x,y);
         System.out.println(" The result is " + result);
+
+        double m = 127.55;
+        double n = 323.99;
+
+        System.out.println("Calculating the sum of " + m + " and " + n);
+        double doubleSum = calculator.sum(m,n);
+        System.out.println("The sum is " + doubleSum);
+
+        System.out.println("Calculating the product of " + m + " and " + n);
+        double doubleProduct = calculator.multiply(m,n);
+        System.out.println("The product is " + doubleProduct);
+
+        System.out.println("Dividing " + m + " by " + n);
+        double doubleResult = calculator.divide(m,n);
+        System.out.println("The result is " + doubleResult);
+
+        System.out.println("Calculating the difference of " + m + " and " + n);
+        double doubleDifference = calculator.minus(m,n);
+        System.out.println("The result is " + doubleDifference);
+
     }
 }
--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,16 +5,33 @@ public class Calculator{
     public int sum(int a, int b){
         return a + b;
     }
+
+    public double sum(double a,double b) {
+        return a + b;
+    }
+
     public int multiply(int a,int b){
         return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    public int divide (int a, int b) {
+        return a / b;
     }
 
     public double divide(double a,double b){
         return a / b;
     }
 
+    public double minus (double a, double b){
+        return a - b;
+    }
+
     public int minus(int a,int b){
         return a - b;
     }
-}
+
+    }
 
