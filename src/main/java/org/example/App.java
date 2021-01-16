b46e9aa2144a890d18a99ac9423fb1c3aee4d949
cmada <madalina.ciceu@gmail.com>
2021-01-16 19:37:54 +0200

    Creare calculator, suma, inmultire si impartire

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class App
+{
+    public static void main(String[] args )
+    {
+       Calculator calculator = new Calculator();
+       int x = 100;
+       int y = 120;
+
+        System.out.println("Calculating the sum of " + x + " and " + y);
+        int sum = calculator.sum(x,y);
+        System.out.println(" The sum is " + sum);
+
+        System.out.println(" Calculating the product of " + x + " and " + y);
+        int product = calculator.multiply(x,y);
+        System.out.println("The product is " + product);
+
+        System.out.println(" Dividing " + x + " and " + y);
+        double result = calculator.divide(x,y);
+        System.out.println(" The result is " + result);
+    }
+}
